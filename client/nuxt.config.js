9c97af76e0beb0fdd618de054e9a6ae852f8db9f
Ivan Vilanculo <vilanculoivan@gmail.com>
2019-10-23 17:42:08 +0200

    apply lint fix

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,31 +1,49 @@
-require('dotenv').config()
+require("dotenv").config();
 
-module.exports = {
-  srcDir: __dirname,
-  env: {
-    API_ENDPOINT: `${process.env.APP_URL}/api`,
-    APP_NAME: process.env.APP_NAME,
-    APP_LOCALE: process.env.APP_LOCALE || 'en'
-  },
-  head: {
-    title: process.env.APP_NAME,
-    titleTemplate: `${process.env.APP_NAME} | Instant Docker Swarm clusters`,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Meta description' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    loading: { color: '#007bff' }
-  },
-  proxy: {
-    '/api': `${process.env.APP_URL}:8000`
-  },
-  router: {
-    middleware: ['laravel']
-  },
-  modules: [['@nuxtjs/axios', { proxy: true, prefix: '/api' }]],
-  buildModules: ['@nuxtjs/tailwindcss']
-}
+export default {
+    srcDir: __dirname,
+    env: {
+        API_ENDPOINT: `${process.env.APP_URL}/api`,
+        APP_NAME: process.env.APP_NAME,
+        APP_LOCALE: process.env.APP_LOCALE || "en"
+    },
+    head: {
+        title: process.env.APP_NAME,
+        titleTemplate: `${process.env.APP_NAME} | Instant Docker Swarm clusters`,
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1"
+            },
+            {
+                hid: "description",
+                name: "description",
+                content: "Meta description"
+            }
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+        loading: { color: "#007bff" }
+    },
+    proxy: {
+        "/api": `${process.env.APP_URL}:8000`
+    },
+    router: {
+        middleware: []
+    },
+    modules: [["@nuxtjs/axios", { proxy: true, prefix: "/api" }]],
+    buildModules: ["@nuxtjs/tailwindcss"],
+    build: {
+        extend(config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: "pre",
+                    test: /\.(js|vue)$/,
+                    loader: "eslint-loader",
+                    exclude: /(node_modules)/
+                });
+            }
+        }
+    }
+};
